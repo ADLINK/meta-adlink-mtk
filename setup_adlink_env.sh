c93a07bd848d38a1f334bc62eb18ddb6d49f4507
aravindlal.mh <aravindlal.mh@adlinktech.com>
2025-10-10 12:46:13 +0530

    Updated setup_adlink_env.sh -m support
    Signed-off-by: aravindlal.mh <aravindlal.mh@adlinktech.com>

--- setup_adlink_env.sh
+++ setup_adlink_env.sh
@@ -26,15 +26,16 @@ Usage: `basename $0` [OPTIONS]
 
 Create build environment
 e.g.
-	source setup_adlink_env.sh -b build_mtk -d /home/adlink/share/downloads -s /home/adlink/share/sstate-cache
+	source setup_adlink_env.sh -b build_mtk -m <MACHINE_NAME> -d /home/adlink/share/downloads -s /home/adlink/share/sstate-cache
 
  -b <BUILD_NAME>	build folder name
+ -m <MACHINE_NAME>	options : "lec-mtk-i1200-qspi","lec-mtk-i1200-ufs","absolute-vision";default if -m not used :lec-mtk-i1200-qspi
  -d <DOWNLOADS>		pre-downloads folder
  -s <SSTATE>		pre-sstate folder
  -h                     Show help
 EOF
 }
-while getopts "b:d:s:" o; do
+while getopts "b:d:s:m:" o; do
     case "${o}" in
     b)
 	BUILD_DIR=${OPTARG}
@@ -47,6 +48,10 @@ while getopts "b:d:s:" o; do
         SSTATE_DIR=$(realpath "$OPTARG")
         export SSTATE_DIR=$SSTATE_DIR
         ;;
+    m)
+        MACHINE=${OPTARG}
+        export MACHINE=$MACHINE
+        ;;
     *)
         usage
 	return
@@ -57,7 +62,7 @@ export TEMPLATECONF=${PWD}/src/meta-adlink-mtk/conf/templates/default
 source src/poky/oe-init-build-env $BUILD_DIR
 export BUILD_DIR=`pwd`
 export DISTRO=rity-demo
-export MACHINE=lec-mtk-i1200-qspi
+export MACHINE=${MACHINE:-lec-mtk-i1200-qspi}
 export BB_ENV_PASSTHROUGH_ADDITIONS="$BB_ENV_PASSTHROUGH_ADDITIONS DL_DIR SSTATE_DIR"
 grep "NDA" ${BUILD_DIR}/conf/local.conf > /dev/null 2>&1 || echo NDA_BUILD = \"1\" >> ${BUILD_DIR}/conf/local.conf 
 
